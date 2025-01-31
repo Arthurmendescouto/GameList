ba7568ff609fc6e7bcfed3574a8cc40cf169ccd8
Arthur <arthurmendescouto16@gmail.com>
2025-01-31 00:01:21 -0300

    Improve CSS for homepage layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,13 @@ import { RedirectCommand, Routes } from '@angular/router';
 import { GameListComponent } from './game-list/game-list.component';
 import { RpggameListComponent } from './rpggame-list/rpggame-list.component';
 import { PlatformGameListComponent } from './platform-game-list/platform-game-list.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
 
-  {
-    path: '',
-     component: GameListComponent},
-    {path:'rpg',
-      component: RpggameListComponent},
-      {path:'platform',
-        component: PlatformGameListComponent}
+  {path: '', component: GameListComponent},
+  {path:'rpg', component: RpggameListComponent},
+  {path:'platform', component: PlatformGameListComponent},
+  {path: 'details/:id', component: DetailsComponent}
 
 ];
--- src/app/rpggame-list/rpggame-list.component.ts
+++ src/app/rpggame-list/rpggame-list.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-game-list',
